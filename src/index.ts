422b2577ba3b147e9e682c8b6c193bb6bb648d22
coyotewind <coyoteswind@gmail.com>
2021-02-10 21:05:34 -0500

    model class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, Newman!');
+// console.log('Hello, Newman!');
+
+import { User } from './models/User';
+const user = new User({});
+
+user.set({ name: 'marcello', age: 52 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+
---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+interface UserProps {
+    name?: string;
+    age?: number;
+}
+
+export class User {
+    constructor(private data: UserProps) {}
+
+    get(propName: string): (string | number) {
+        return this.data[propName];
+    }
+
+    set(update: UserProps): void {
+        Object.assign(this.data, update);
+    }
+
+}
+
+// new User({ name: 'john', age: 51});
